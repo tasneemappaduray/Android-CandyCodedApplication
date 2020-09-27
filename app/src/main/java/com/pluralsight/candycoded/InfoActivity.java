4b3c34709b54ace25c28e784af8fad286a047afc
Tasneem <tasneemappaduray@gmail.com>
2020-09-27 21:04:11 +0200

    Added createPhoneIntent

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -40,4 +40,10 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view){
+        Intent callIntent = new Intent(Intent.ACTION_DIAL);
+        Uri number = Uri.parse("tel:0123456789");
+        callIntent.setData(number);
+        startActivity(callIntent);
+    }
 }
